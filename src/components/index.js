4c9c19471a8c14bee94ae166074f29a666880db1
Luiz Pedro <luizpsmoreira@gmail.com>
2020-10-29 23:12:58 -0300

    Updates

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import About from "./About";
-import Contacts from "./Contacts";
-import Header from "./Header";
-import Projects from "./Projects";
-import Landing from "./Landing";
-import Resume from "./Resume";
-
-export { About, Contacts, Projects, Header, Landing, Resume };
