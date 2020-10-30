97748a8b5d1fb2d0aa99f98fc3d19350709772b3
Luiz Pedro <luizpsmoreira@gmail.com>
2020-10-29 23:37:34 -0300

    Updates

---
+++ precache-manifest.a2871cb732555482d60dc8f3d250d077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df245472c46c925c5559ef6d950f0623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db8605f607154b4e9f5a",
+    "url": "/static/js/2.ebf6e141.chunk.js"
+  },
+  {
+    "revision": "0efabacf1d5a43cdae1da6fa187843ce",
+    "url": "/static/js/2.ebf6e141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63ab9e7c706267ec2dd",
+    "url": "/static/js/main.367726b8.chunk.js"
+  },
+  {
+    "revision": "25adb8eba63c9e197782",
+    "url": "/static/js/runtime-main.c46e58d3.js"
+  }
+]);
